a863af78bf928581298dcda3a105fe624425a348
jhleezz00 <wldusdkwkd@gmail.com>
2022-05-14 13:57:58 +0900

    main

--- 챗봇/챗봇테스트.py
+++ 챗봇/챗봇테스트.py
@@ -16,6 +16,7 @@ conn = pymysql.connect(host='localhost', user='root', password='1234',db='chatbo
 userQuest = input("궁금한것을 물어보세요! ")
 nouns=okt.nouns(userQuest) #질문 문장 형태소 분석
 print(nouns)
+print("수정")
 
 #키워드가 데이터베이스 안에 있는지 찾고 있으면 답변 출력하는 반복문
 while True: 
@@ -34,7 +35,7 @@ while True:
                         print(result) 
                  
             else:             
-                print("일치하는 답변이 없습니다.") #키워드가(혹은 일치하는 답변이) 데이터베이스 안에 없을 경우
+                print("일치하는 답변이 없습니다") #키워드가(혹은 일치하는 답변이) 데이터베이스 안에 없을 경우
                 break
         else: #nouns[i]가 null값일 경우
         
